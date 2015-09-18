37b2c6f761c42325c8ccc3993524c0cd4bd74087
Mark Final <markfinal@hotmail.com>
2015-09-18 21:51:48 +0100

    [tests] Adding test harness configuration

---
+++ tests/bamtests.py
@@ -0,0 +1,8 @@
+from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+
+def ConfigureRepository():
+    configs = {}
+    configs["zeromqtest"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]},
+                                      linux={"Native":[gcc64],"MakeFile":[gcc64]},
+                                      osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
+    return configs
