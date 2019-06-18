56c949a66576b782fd1a6d1d9e55629a26854f24
Mark Final <markfinal@hotmail.com>
2019-06-18 22:28:32 +0100

    [CI] Starting tests

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -1,8 +1,8 @@
-from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+from testconfigurations import TestSetup, visualc, visualc64, visualc32, mingw32, gcc, gcc64, gcc32, clang, clang32, clang64
 
 def configure_repository():
     configs = {}
-    configs["zeromqtest"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]},
-                                      linux={"Native":[gcc64],"MakeFile":[gcc64]},
-                                      osx={"Native":[clang64],"MakeFile":[clang64],"Xcode":[clang64]})
+    configs["zeromqtest"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                      linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                      osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
